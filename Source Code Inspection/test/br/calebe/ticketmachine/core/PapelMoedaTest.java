8d4d66ff409364dafc1222eefcc64e1b0830b85e
Gabriel Lopes <unifgabsantos@fei.edu.br>
2023-05-05 22:13:18 -0300

    Update

---
+++ Source Code Inspection/test/br/calebe/ticketmachine/core/PapelMoedaTest.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/UnitTests/JUnit4TestClass.java to edit this template
+ */
+package br.calebe.ticketmachine.core;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author unifgabsantos
+ */
+public class PapelMoedaTest {
+    
+    public PapelMoedaTest() {
+    }
+
+}
---
+++ Source Code Inspection/test/br/calebe/ticketmachine/core/TrocoTest.java
@@ -0,0 +1,41 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/UnitTests/JUnit4TestClass.java to edit this template
+ */
+package br.calebe.ticketmachine.core;
+
+import java.util.Iterator;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author unifgabsantos
+ */
+public class TrocoTest {
+    /**
+     * Test of getIterator method, of class Troco.
+     */
+    
+    @Test
+    public void verificarQuantidade() {
+        int quantity = 52;
+        PapelMoeda papelMoeda = new PapelMoeda(10, quantity);
+        System.out.println(papelMoeda);
+        assertEquals(papelMoeda.getQuantidade(), quantity);
+        fail("Este erro era esperado.");
+    } 
+    
+    @Test
+    public void verificarValor() {
+        int value = 52;
+        PapelMoeda papelMoeda = new PapelMoeda(value, 10);
+        assertEquals(papelMoeda.getValor(), value);
+    }   
+ 
+    
+}
